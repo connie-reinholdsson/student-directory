ceebc2f140eee83ef6324860e427ab01a642c549
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-03-02 18:04:58 +0000

    Updated directory to load from command line

--- directory.rb
+++ directory.rb
@@ -1,27 +1,5 @@
 @students = [] #an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #get the first name
-  name = gets.chomp
-  #while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-end
-end
-
 def print_menu
 puts "1. Input the students"
 puts "2. Show the students"
@@ -30,10 +8,12 @@ puts "4. Load the list from students.csv"
 puts "9. Exit" # 9 because we'll be adding more items
 end
 
-def show_students
-print_header
-print_students_list
-print_footer
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -49,7 +29,28 @@ def process(selection)
   when "9"
     exit #this will cause the program to terminate
   else puts "I don't know what you meant, try again"
+  end
 end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #get the first name
+  name = STDIN.gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    #get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+print_header
+print_students_list
+print_footer
 end
 
 def print_header
@@ -84,8 +85,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r") #Set variable 'file' to open file with read access.
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r") #Set variable 'file' to open file with read access.
   file.readlines.each do |line| #Read all the lines into an array. .readlines it a method.
     #and iterate over it.
     name, cohort = line.chomp.split(',') #On every iteration,
@@ -98,7 +99,29 @@ end
 file.close
 end
 
+#The method has a file name hard-coded into it i.e. students.csv.
+#We need to make it more flexibile, but it make the original functionalty work,
+#let's give it a default value.
+#This means, the method accepts the file as an argument.
+#But if there is no argument, it will use students.csv.
+
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quit the program
+  end
+end
 
 
+#.exists? is a method defined in class File.
+#Adding $stdin.gets to ensure it works with ARGV.
+#.gets reads from the list of files supplied as arguments,
+#so we need to direct it to the input stream.
 
+try_load_students
 interactive_menu
